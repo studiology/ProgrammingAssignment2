6220585a418daaaaf1303e0e107a88949fc5d161
studiology <adamk@studiology.nu>
2015-06-17 15:30:18 +1000

    moved test functions from cachematrix.R to tests.R

--- tests.R
+++ tests.R
@@ -1,3 +1,4 @@
+###############################
 ## Matrix solution test functions sourced from Steven Okamoto in this post:
 ## https://class.coursera.org/rprog-015/forum/thread?thread_id=442#post-2918
 
@@ -95,4 +96,54 @@ run.test <- function(n, rng=1:10) {
                                                 ident=ident)))
         }
         results
+}
+
+###############################################################################
+
+## These functions are used for testing makeCacheMatrix and cacheSolve only
+## hilbert : generate a solveable n*n matrix (only useful up to 11x11)
+## checkit : verifies whether two matrices are inverse of eachother
+## multicheck : creates a new n*n matrix, then prints, caches, solves, prints the solution, then verifies it
+
+hilbert <- function(n=4L) {
+        ## generates an n*n (4x4 by default) Hilbert matrix
+
+        ## Note that 12x12 matrices (and larger) are not solveable on most
+        ## machines because the fractions get too small for the computer to be
+        ## able to tell them apart: see .Machine$double.eps
+        i <- 1:n
+        1 / outer(i - 1, i, "+")
+}
+
+
+checkit <- function(mat1, mat2) {
+        ## multiply mat1 by mat2 and compare with identity matrix of the same size
+        ## returns TRUE if mat2 is the inverse of mat1
+        ## mat1 can be a matrix or a list created by makeCacheMatrix
+
+        ## If mat1 is a list, then mat2 is optional and mat1$get() and
+        ## mat1$getsol() will be used for verification
+        if (class(mat1) == "list") {
+                m1 <- mat1$get()
+                m2 <- mat1$getsol()
+        } else {
+                m1 <- mat1
+                m2 <- mat2
+        }
+        n <- dim(m1)[1]
+        ident <- diag(n)
+        identical(round(m1 %*% m2),ident)
+}
+
+testCacheSolve <- function(n) {
+        a <- hilbert(n)
+        #print(round(a,2))
+
+        cm <- makeCacheMatrix(a)
+        cs <- cacheSolve(cm)
+
+        #print(round(cs,2))
+        ident <- diag(dim(a)[1])
+        is_inverse <- checkit(a,cs)
+        list(size = n, matrix1 = a, inverse = cs, identity = ident, is_inverse = is_inverse)
 }
